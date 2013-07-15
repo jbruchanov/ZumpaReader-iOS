8594b4c08f43312a872812a9c0ffcf2da6f75919
Jiri Bruchanov <jbruchanov@gmail.com>
2013-07-15 20:49:56 +0100

    Initial Commit

---
+++ zumpareader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zumpareader
+//
+//  Created by Joe Scurab on 7/15/13.
+//  Copyright (c) 2013 Jiri Bruchanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ zumpareaderTests/zumpareaderTests.m
@@ -0,0 +1,32 @@
+//
+//  zumpareaderTests.m
+//  zumpareaderTests
+//
+//  Created by Joe Scurab on 7/15/13.
+//  Copyright (c) 2013 Jiri Bruchanov. All rights reserved.
+//
+
+#import "zumpareaderTests.h"
+
+@implementation zumpareaderTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in zumpareaderTests");
+}
+
+@end
