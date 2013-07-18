4995eb41df4d3b7b30d0df85ef061e0ad8641104
Jiri Bruchanov <jbruchanov@gmail.com>
2013-07-18 21:22:19 +0100

    Update ImageEditor

--- zumpareader/ViewControllers/ImageEditorViewController.m
+++ zumpareader/ViewControllers/ImageEditorViewController.m
@@ -8,6 +8,8 @@
 
 #import "ImageEditorViewController.h"
 
+#define JPEG_QUALITY 80
+
 @interface ImageEditorViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *originalResolution;
 @property (weak, nonatomic) IBOutlet UILabel *originalSize;
@@ -40,61 +42,76 @@
     // Dispose of any resources that can be recreated.
 }
 
-int rotation;
-- (IBAction)rotateDidClick:(id)sender {    
+UIImageOrientation rotation = UIImageOrientationUp;
+
+- (IBAction)rotateDidClick:(id)sender {
+    rotation = [self nextRotation:rotation];
     self.updatedImage = [[UIImage alloc] initWithCGImage: self.updatedImage.CGImage
                                                    scale: 1.0
-                                             orientation: rotation++];
+                                             orientation: rotation];
     
     self.imageView.image = self.updatedImage;
     [self updateStats];
 }
 
+-(UIImageOrientation) nextRotation:(UIImageOrientation) current{
+    switch(current){
+        default:
+        case UIImageOrientationUp:
+            return UIImageOrientationRight;
+        case UIImageOrientationRight:
+            return UIImageOrientationDown;
+        case UIImageOrientationDown:
+            return UIImageOrientationLeft;
+        case UIImageOrientationLeft:
+            return UIImageOrientationUp;
+    }
+}
+
 -(void)updateStats{
     
     NSString *res = [NSString stringWithFormat:@"%dx%d", (int)self.image.size.width, (int)self.image.size.height];
-    NSString *size = [NSString stringWithFormat:@"%d KiB", -1];
+    NSData* imageData = [NSData dataWithData:UIImageJPEGRepresentation(self.image, JPEG_QUALITY)];
+    NSString *size = [NSString stringWithFormat:@"%d KiB", ([imageData length] / 1000)];
     self.originalResolution.text = res;
     self.originalSize.text = size;
     
     if(self.updatedImage){
         NSString *res = [NSString stringWithFormat:@"%dx%d", (int)self.updatedImage.size.width, (int)self.updatedImage.size.height];
-        NSString *size = [NSString stringWithFormat:@"%d KiB", -1];
+        NSData* imageData = [NSData dataWithData:UIImageJPEGRepresentation(self.updatedImage, JPEG_QUALITY)];
+        NSString *size = [NSString stringWithFormat:@"%d KiB", ([imageData length] / 1000)];
         self.updatedResolution.text = res;
         self.updatedSize.text = size;
     }else{
         self.updatedResolution.text = @"";
         self.updatedSize.text = @"";
     }
-    
-    
 }
 
 - (IBAction)resizeDidClick:(UISegmentedControl *)sender {
-    float scale = 1;
-    switch (sender.selectedSegmentIndex) {
-        default:
-        case 0:
-            scale = 1;
-            break;
-        case 1:
-            scale = 2;
-            break;
-        case 2:
-            scale = 4;
-            break;
-        case 3:
-            scale = 8;
-            break;
+    float scale = 1 << sender.selectedSegmentIndex;
+    if(scale == 1){
+        self.updatedImage = self.image;
+        rotation = UIImageOrientationUp;
+    }else{
+        self.updatedImage = [self imageWithImage:self.image scaleRatio: 1 / scale];
     }
-    
-    self.updatedImage = [[UIImage alloc] initWithCGImage: self.image.CGImage
-                                                   scale: scale
-                                             orientation: rotation];
     
     self.imageView.image = self.updatedImage;
     [self updateStats];
+}
 
+- (UIImage*)imageWithImage:(UIImage*)image
+              scaleRatio:(float)ratio
+{
+    CGSize origSize = image.size;
+    CGSize newSize = CGSizeMake(origSize.width * ratio, origSize.height * ratio);
+    UIGraphicsBeginImageContext( newSize );
+    [image drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
 }
 
 @end
